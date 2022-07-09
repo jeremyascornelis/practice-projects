af657d07f61569e6f9e361b8fa94fb99b7e4f3ee
Jeremyas Cornelis <jeremyas.cornelis@gmail.com>
2022-07-09 13:57:11 +0700

    setup home, init, config

---
+++ toko-abc/app/views/home/index.php
@@ -0,0 +1,6 @@
+<div class="container ps-4">
+    <div class="jumbotron mt-4">
+        <h1 class="display-4"><b>Home</b></h1>
+        <p class="fs-4">Selamat Datang di ABC Bag Store</p>
+    </div>
+</div>
