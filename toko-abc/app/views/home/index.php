1523da187a5012174d4333bf6977e1b779970d21
Jeremyas Cornelis <jeremyas.cornelis@gmail.com>
2022-07-09 15:34:59 +0700

    display products in home [success]

--- toko-abc/app/views/home/index.php
+++ toko-abc/app/views/home/index.php
@@ -1,15 +1,16 @@
 <div class="container mt-3 mb-5 ps-4">
 <div class="row">
-<h1 class="mb-4">Daftar Produk</h1>
+<h1 class="mb-4 text-center" style="color:#21313C;">Selamat Berbelanja</h1>
     <?php foreach ($data['produk'] as $p) : ?>
         <div class="col-4 mb-3">
-            <div class="card" style="width: 18rem;">
+            <div class="card shadow p-3 mb-5 bg-white rounded" >
                 <img class="card-img-top" src="<?= BASEURL; ?>/img/<?= $p['gambar']; ?>" alt="Card image cap">
                 <div class="card-body">
-                    <h5 class="card-title"><?= $p['nama_produk']; ?></h5>
-                    <p class="card-text">Harga <?= number_format($p['harga']); ?></p>
+                    <h5 class="card-title fs-3"><?= $p['nama_produk']; ?></h5>
+                    <p class="card-text fs-5"><?= $p['deskripsi'];?></p>
+                    <h6 class="card-text fs-4 mb-4">Rp. <?= number_format($p['harga']); ?></h6>
                     <!-- <a href="<?= BASEURL; ?>/produk/addcart/<?= $p['id'];?>" class="btn btn-primary">Order</a> -->
-                    <a href="#" class="btn btn-primary">Order</a>
+                    <a href="#" class="btn btn-primary text-center fs-5" >Masukkan Keranjang</a>
                 </div>
             </div>
         </div>
--- toko-abc/app/views/templates/header.php
+++ toko-abc/app/views/templates/header.php
@@ -4,14 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Halaman <?= $data['judul']; ?></title>
+    <title><?= $data['judul']; ?></title>
     <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We' crossorigin='anonymous'>
 </head>
 <body>
 <div class="container">
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
-            <h1><a href="<?= BASEURL;?>" class="navbar-brand mb-0 h1 fs-2" style="font-size: 30px"><b>ABC Bag Store</b></a></h1>
+            <h1><a href="<?= BASEURL;?>" class="navbar-brand mb-0 h1 fs-2" style="font-size: 30px; color:#21313C;"><b>ABC Bag Store</b></a></h1>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
